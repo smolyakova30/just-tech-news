a2cbe1f814f350cd306eb6596c7de1495b89ad6f
Olga <o.smolyakova30@gmail.com>
2021-08-24 17:37:49 -0500

    added handlebars

--- models/Vote.js
+++ models/Vote.js
@@ -1,10 +1,15 @@
+// import model and dataTypes from the sequelize module
 const { Model, DataTypes } = require('sequelize');
+// importing the the connection file
 const sequelize = require('../config/connection');
 
+// crreate Vote Model
 class Vote extends Model {}
 
+//initiailizing the vote model with fields/columns
 Vote.init(
   {
+    //define the titile column w=ith a given data type and validations
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
@@ -27,6 +32,8 @@ Vote.init(
       }
     }
   },
+
+  // define the optionfor the database
   {
     sequelize,
     timestamps: false,
@@ -36,4 +43,5 @@ Vote.init(
   }
 );
 
+//make it visible for all apllication 
 module.exports = Vote;
