6bad547e5e0c57ee71589b1ff3ed666cc9071f02
Olga <o.smolyakova30@gmail.com>
2021-08-27 19:13:13 -0500

    last updates

--- models/Vote.js
+++ models/Vote.js
@@ -1,15 +1,10 @@
-// import model and dataTypes from the sequelize module
 const { Model, DataTypes } = require('sequelize');
-// importing the the connection file
 const sequelize = require('../config/connection');
 
-// crreate Vote Model
 class Vote extends Model {}
 
-//initiailizing the vote model with fields/columns
 Vote.init(
   {
-    //define the titile column w=ith a given data type and validations
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
@@ -17,7 +12,6 @@ Vote.init(
     },
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'user',
         key: 'id'
@@ -25,15 +19,12 @@ Vote.init(
     },
     post_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'post',
         key: 'id'
       }
     }
   },
-
-  // define the optionfor the database
   {
     sequelize,
     timestamps: false,
@@ -43,5 +34,4 @@ Vote.init(
   }
 );
 
-//make it visible for all apllication 
 module.exports = Vote;
